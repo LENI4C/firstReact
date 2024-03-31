50791c2dc995bb81fc6806fa09cbff75f8016ad3
LENI4C <timibraperetei74@gmail.com>
2024-03-31 15:53:27 +0100

    init commit

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.querySelector("#container"))
+
+const Header = () => {
+  return (
+    <p> Hello there, this is the header</p>
+  )
+}
+
+const Main = () => {
+  return (
+    <div className="mainContainer">
+      <div className="card1"> Card 1</div>
+      <div className="card2">Card 2</div>
+      <div className="card3">Card 3</div>
+    </div>
+  )
+}
+
+root.render(
+    <React.StrictMode>
+        <Header />
+        <Main />
+    </React.StrictMode>
+);
+
