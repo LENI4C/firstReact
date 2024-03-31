bc5e45a07eed0070ebbff8a3cd2de9fa6ebbb22f
LENI4C <timibraperetei74@gmail.com>
2024-03-31 17:15:15 +0100

    layout tweak

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.querySelector("#container"))
+const root = ReactDOM.createRoot(document.querySelector("#container"));
 
 const Header = () => {
-  return (
-    <p> Hello there, this is the header</p>
-  )
-}
+    return <p> Hello there, this is the header</p>;
+};
 
 const Main = () => {
-  return (
-    <div className="mainContainer">
-      <div className="card1"> Card 1</div>
-      <div className="card2">Card 2</div>
-      <div className="card3">Card 3</div>
-    </div>
-  )
-}
+    return (
+        <div className="mainContainer">
+            <div className="card1"> Card 1</div>
+            <div className="card2">Card 2</div>
+            <div className="card3">Card 3</div>
+        </div>
+    );
+};
 
 root.render(
     <React.StrictMode>
@@ -25,4 +23,3 @@ root.render(
         <Main />
     </React.StrictMode>
 );
-
